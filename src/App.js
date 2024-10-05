601264370a4c7739d45ac00d8c85e8b9ea2a3852
Pooja <poojamagna2022@gmail.com>
2024-10-05 11:41:00 +0530

    search option

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Header from "./components/Header";
 import Index from "./components/index";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import Search from "./components/Search";
 
 function App() {
 
@@ -17,11 +18,12 @@ function App() {
     <Router>
       <Header/>
       <div className="float">
-<a href="tel:+01823222674"><img src={float} alt="24/7 services sandhu hospital" /></a>
+      <a href="tel:+01823222674"><img src={float} alt="24/7 services sandhu hospital" /></a>
     </div>
       <Routes>
         <Route exact path="/" element={<Index/>} />
         <Route exact path="/about" element={<About/>} />
+        <Route exact path="/search" element={<Search/>} />
       </Routes>
       <Footer/>
     </Router>
